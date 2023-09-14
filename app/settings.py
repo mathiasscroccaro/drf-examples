f53fb73f69858d664e0ab4c34d145ae5f234123a
Mathias <mathias.scroccaro@gmail.com>
2023-09-14 20:00:48 -0300

    Add FORCE_SCRIPT_NAME configuration

--- app/settings.py
+++ app/settings.py
@@ -31,6 +31,8 @@ ENVIRONMENT = os.getenv("ENVIRONMENT", "production").lower()
 
 ALLOWED_HOSTS = ['*'] if ENVIRONMENT == "test" else ['app.mathias.dev.br']
 
+# Forces django to not strip the /drf-example from the URI
+FORCE_SCRIPT_NAME = '/drf-example/'
 
 # Application definition
 
@@ -121,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "drf-example/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
