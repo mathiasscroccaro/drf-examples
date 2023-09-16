42134af4040788add59107599340e6554b65d96f
Mathias <mathias.scroccaro@gmail.com>
2023-09-15 22:35:45 -0300

    Change STATIC_ROOT

--- app/settings.py
+++ app/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = "/drf-example/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = "/mnt/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
