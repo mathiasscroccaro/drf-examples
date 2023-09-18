eeeb53e1062dda729fea8e233675e11095e6bb6a
Mathias <mathias.scroccaro@gmail.com>
2023-09-18 18:42:53 -0300

    Add /drf-examples to STATIC_URL

--- app/settings.py
+++ app/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/drf-example/static/"
 
 STATIC_ROOT = "/mnt/static"
 
