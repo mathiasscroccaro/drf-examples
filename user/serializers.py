d40e48fa9677bd045a5be7109757bcaf9a853dda
Mathias <mathias@devgrid.co.uk>
2023-09-19 20:17:20 -0300

    Implement serializers for User login

---
+++ user/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import (
+    get_user_model,
+)
+from django.utils.translation import gettext as _
+
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object."""
+
+    password = serializers.CharField(
+        max_length=250, allow_blank=True, write_only=True
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'password', 'name']
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        """Create and return a user with encrypted password."""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class SessionLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
